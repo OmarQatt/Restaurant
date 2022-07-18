826ca7c50f7b0d235af09c2e0399e6c5690b4dd1
Omar Qattam <omarqattam99@gmail.com>
2022-07-18 23:59:28 +0300

    Restaurant

--- statistics.js
+++ statistics.js
@@ -50,3 +50,85 @@ table.innerHTML += row
 getData()
 
 
+
+
+function pieChar() {
+  
+let map = new Map();
+for (let i = 0; i < allFood.length; i++) {
+  if(!map.has(allFood[i].type)){
+   map.set(allFood[i].type, 1)
+  } else {
+    map.set(allFood[i].type, (map.get(allFood[i].type) + 1))
+  }
+}
+  var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+  ];
+  
+  const myChart= new Chart("myChart", {
+    type: "pie",
+    data: {
+      datasets: [{
+        backgroundColor: barColors,
+        data: Array.from(map.values()),
+      }],
+      labels:Array.from(map.keys()) 
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+      }
+    }
+  });
+}
+
+
+function barChart(){
+const names = [];
+const prices = [];
+for (let i = 0; i < allFood.length; i++) {
+  names.push(allFood[i].foodName);
+  prices.push(allFood[i].price)
+}
+
+  // var xValues = ["Italy", "France", "Spain"];
+ // var yValues = [55, 49, 44, 24, 15];
+var barColors = ["red", "green","blue","orange","brown"];
+
+const myChart = new Chart("myChartBar", {
+  type: "bar",
+  data: {
+    labels: names,
+    datasets: [{
+      backgroundColor: barColors,
+      data: prices
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "World Wine Production 2018"
+    },
+    scales: {
+      yAxes: [{
+          ticks: {
+              beginAtZero: true
+          }
+      }]
+  }
+    
+  }
+});
+}
+pieChar()
+barChart()
+
+
+      
